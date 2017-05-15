5b2bc36f00f08b26c40f443aa9a4977e5a8eeb0d
jerik0 <jcarrico1985@live.com>
2017-05-15 14:57:27 -0500

    Added methods: sale(), print(), and exit(). Added a basic UI. Also added a switch for options to be chosen.

--- src/UnicorStore.java
+++ src/UnicorStore.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 
 public class UnicorStore {
 
+    static Scanner scan = new Scanner(System.in);
+
+
+    private static void displayMenu() {
+        System.out.println("Welcome to Unicorn Exports!");
+        System.out.println("Choose an option:");
+        System.out.println("---------------------------");
+        System.out.println("1) Sale");
+        System.out.println("2) Print");
+        System.out.println("3) Exit");
+        int option = scan.nextInt();
+        switch (option) {
+            case 1: {
+                sale();
+            } break;
+            case 2: {
+                print();
+            } break;
+            default: {
+                System.exit(0);
+            }
+        }
+    }
+
+    private static void sale() {
+
+    }
+
+    private static void print() {
+
+    }
+
+    private static void exit() {
+
+    }
+
+    public static void main() {
+
+        displayMenu();
+
+    }
 
 }
